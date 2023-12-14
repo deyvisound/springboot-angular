e952217970a494ade161437cddec7f8fce74f19c
Deyvison Estevam <deyvisonestevam@gmail.com>
2023-12-13 23:34:24 -0300

    feat: adicionando componente de cadastro

--- usermanager-cli/src/app/app.routes.ts
+++ usermanager-cli/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UserlistComponent } from './userlist/userlist.component';
+import { UserformComponent } from './userform/userform.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ 
+    {path: 'user-list', component: UserlistComponent},
+    {path: 'user-form', component: UserformComponent}
+];
+
--- usermanager-cli/src/app/userlist/userlist.component.ts
+++ usermanager-cli/src/app/userlist/userlist.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-userlist',
@@ -10,10 +11,16 @@ import { Component } from '@angular/core';
 })
 export class UserlistComponent {
 
+  constructor(private router: Router){ }
+
+  getUserForm() {
+    this.router.navigate(['/user-form']);
+  }
+
   users = [
     { id: 1, name: 'Carla', email: 'teste@email.com' },
     { id: 2, name: 'Fernando', email: 'teste@email.com' },
-    { id: 3, name: 'Ana',  email: 'teste@email.com' },
+    { id: 3, name: 'Ana', email: 'teste@email.com' },
     { id: 4, name: 'Denis', email: 'teste@email.com' },
     { id: 5, name: 'Alice', email: 'teste@email.com' },
     { id: 6, name: 'Antonio', email: 'teste@email.com' }
